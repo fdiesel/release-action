3249c4d74af5ea6d49128666e0a677080f4fc6b2
fdiesel <florian.diesel@posteo.de>
2024-09-25 18:22:28 +0200

    fix: create major tag

--- src/post.ts
+++ src/post.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { execSync } from 'child_process';
 import { parseStrategy, Strategy } from './strategy';
+import { Tag } from './tag';
 
 const token = core.getInput('token');
 const strategy = core.getInput('strategy')
@@ -23,21 +24,48 @@ async function getLatestCommitSha(): Promise<string> {
 
 async function run() {
   if (!releaseId || !nextVersion) return;
+  const nextTag = new Tag(nextVersion);
+
+  // strategies
   switch (strategy) {
     case Strategy.NODE:
       execSync(`git config user.name "GitHub Actions"`);
       execSync(`git config user.email "action@github.com"`);
       execSync(
-        `npm version ${nextVersion} -m "chore(node): bump version to %s" --allow-same-version`
+        `npm version ${nextTag.version.toString()} -m "chore(node): bump version to %s" --allow-same-version`
       );
       execSync(`git push origin HEAD:${github.context.ref}`);
       break;
   }
+
+  const latestCommitSha = await getLatestCommitSha();
+
+  // major tag
+  const majorTagName = nextTag.toMajorString();
+  const { data: majorTag } = await octokit.rest.git.getRef({
+    ...repo,
+    ref: `tags/${majorTagName}`
+  });
+  if (majorTag) {
+    await octokit.rest.git.updateRef({
+      ...repo,
+      ref: `tags/${majorTagName}`,
+      sha: latestCommitSha,
+    });
+  } else {
+    await octokit.rest.git.createRef({
+      ...repo,
+      ref: `refs/tags/${majorTagName}`,
+      sha: latestCommitSha,
+    });
+  }
+
+  // release
   const { data } = await octokit.rest.repos.updateRelease({
     ...repo,
     release_id: parseInt(releaseId),
     draft: false,
-    target_commitish: await getLatestCommitSha()
+    target_commitish: latestCommitSha
   });
   core.info(`Release created: ${data.html_url}`);
 }
