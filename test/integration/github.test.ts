6e574f4eacd276a22c673480914a51433d6f6fdc
fdiesel <florian.diesel@posteo.de>
2024-10-01 08:45:51 +0000

    fix: retrieve sha on every test

--- test/integration/github.test.ts
+++ test/integration/github.test.ts
@@ -1,27 +1,25 @@
 import { describe, expect, test } from '@jest/globals';
 import { randomUUID } from 'crypto';
-import { afterEach, before } from 'node:test';
+import { afterEach } from 'node:test';
 import { inputs } from '../../src/inputs';
 import { Provider } from '../../src/lib/providers';
 import { Ref, RefTypes } from '../../src/lib/ref';
 import { GitHubProvider } from '../../src/providers/github';
 
 const provider: Provider<unknown> = new GitHubProvider(inputs.token);
-let sha: string = '';
 
 const refStore: Ref<RefTypes>[] = [];
 
-function generateRef<Type extends RefTypes>(type: Type): Ref<Type> {
+async function generateParams<Type extends RefTypes>(
+  type: Type
+): Promise<{ ref: Ref<Type>; sha: string }> {
   const ref = new Ref(type, `test-${randomUUID()}`);
   refStore.push(ref);
-  return ref;
+  const sha = await provider.getLatestCommitSha();
+  return { ref, sha };
 }
 
 describe('github', () => {
-  before(async () => {
-    sha = await provider.getLatestCommitSha();
-  });
-
   afterEach(async () => {
     for (const ref of refStore) {
       switch (ref.type) {
@@ -35,21 +33,26 @@ describe('github', () => {
     }
   });
 
+  test('should get latest commit sha', async () => {
+    const latestCommitSha = await provider.getLatestCommitSha();
+    expect(latestCommitSha).toBeTruthy();
+  });
+
   test('should check a tag', async () => {
-    const ref = generateRef(RefTypes.TAGS);
+    const { ref, sha } = await generateParams(RefTypes.TAGS);
     await provider.tags.create(ref, sha);
     expect(await provider.tags.exists(ref)).toBe(true);
   });
 
   test('should creating a tag', async () => {
-    const ref = generateRef(RefTypes.TAGS);
+    const { ref, sha } = await generateParams(RefTypes.TAGS);
     expect(async () => {
       provider.tags.create(ref, sha);
     }).not.toThrow();
   });
 
   test('should update a tag', async () => {
-    const ref = generateRef(RefTypes.TAGS);
+    const { ref, sha } = await generateParams(RefTypes.TAGS);
     await provider.tags.create(ref, sha);
     expect(async () => {
       provider.tags.update(ref, sha);
