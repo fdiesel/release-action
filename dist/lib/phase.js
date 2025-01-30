fee445eb89348d139a61cd9c2626642c99e15d38
fdiesel <florian.diesel@posteo.de>
2025-01-30 10:16:38 +0000

    test

--- dist/lib/phase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Phase = void 0;
-var Phase;
-(function (Phase) {
-    Phase["Dev"] = "dev";
-    Phase["Prod"] = "prod";
-})(Phase || (exports.Phase = Phase = {}));
